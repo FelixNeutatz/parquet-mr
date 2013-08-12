7800c2e6823eb32734610780b1e56b06ff05761c
FelixNeutatz <neutatz@googlemail.com>
2013-08-12 16:44:56 -0700

    This class is needed to be able to read two Parquet files with different schemas at once
    I need this class here: TestProjectedInputOutputFormat.java

---
+++ parquet-hadoop/src/test/java/parquet/hadoop/example/FirstInputFormat.java
@@ -0,0 +1,9 @@
+package parquet.hadoop.example;
+
+import parquet.hadoop.thrift.ParquetThriftInputFormat;
+
+public class FirstInputFormat extends ParquetThriftInputFormat{
+	public FirstInputFormat(){
+		super();
+	}
+}
